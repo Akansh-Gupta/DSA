9dd327e717d702cd961edbd4a73f8eb635d8cbf6
Akansh-Gupta <akg.gupta365@gmail.com>
2026-05-30 16:52:35 +0530

    Added VowelsCount

---
+++ String/VowelsCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VowelsCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.toLowerCase().charAt(i);
+            if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u') count++;
+        }
+        System.out.println("No. of vowels: " + count);
+        sc.close();
+    }
+}
