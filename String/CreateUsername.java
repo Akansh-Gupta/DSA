fbd956763c1016f6a79b76fa012f4b2091d109f4
Akansh-Gupta <akg.gupta365@gmail.com>
2026-05-30 16:39:15 +0530

    Added CreateUsername

---
+++ String/CreateUsername.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CreateUsername {
+    static String[] extractName(String name){
+        String parts[] = name.trim().toLowerCase().split("\\s+");  // "\\s+" splits everywhere there is a space  
+        return parts;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Full Name: ");
+        String fullName = sc.nextLine();
+        String name[] = extractName(fullName);
+        String firstName = name[0];
+        String lastName = name[name.length-1];
+        System.out.println("Enter Student ID: ");
+        String studentID = sc.nextLine();
+        System.out.println(firstName + "_" + lastName + studentID);
+        sc.close();
+    }
+}
