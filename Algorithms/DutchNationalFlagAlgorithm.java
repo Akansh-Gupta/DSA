2b4c730c2ab97aabc6bb86b36101636169c17051
Akansh-Gupta <akg.gupta365@gmail.com>
2026-05-31 15:49:46 +0530

    Merge issue resolved

--- Algorithms/DutchNationalFlagAlgorithm.java
+++ Algorithms/DutchNationalFlagAlgorithm.java
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-
-public class DutchNationalFlagAlgorithm {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.print("Enter values (0,1,2): ");
-        for (int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-        int l = 0, m = 0, h = n - 1;
-        while (m <= h) {
-            if (arr[m] == 0) {
-                int temp = arr[l];
-                arr[l] = arr[m];
-                arr[m] = temp;
-                l++; m++;
-            } else if (arr[m] == 1) {
-                m++;
-            } else {
-                int temp = arr[m];
-                arr[m] = arr[h];
-                arr[h] = temp;
-                h--;
-            }
-        }
-        System.out.println("Sorted Array: ");
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i] + " ");
-        }
-        sc.close();
-    }
-}
-=======
 import java.util.Scanner;
 
 public class DutchNationalFlagAlgorithm {
