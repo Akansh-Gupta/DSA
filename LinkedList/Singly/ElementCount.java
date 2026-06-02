9d405f77d6ef9faf14eaa3ddea7ea3c5c0e5e295
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-02 21:41:40 +0530

    Added ElementCount

---
+++ LinkedList/Singly/ElementCount.java
@@ -0,0 +1,24 @@
+public class ElementCount {
+    static int count(SinglyLinkedList list){
+        Node temp = list.head;
+        int count = 0;
+        while (temp!=null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+        list.insertAtEnd(60);
+        list.insertAtEnd(70);
+        list.insertAtEnd(80);
+
+        System.out.println("No. of elements : " + count(list));
+    }
+}
