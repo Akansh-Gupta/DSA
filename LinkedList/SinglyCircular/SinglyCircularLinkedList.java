24902841235e0242c38aa19b928c6d8bca27060c
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-02 23:22:42 +0530

    Added SinglyCircularLinkedList

---
+++ LinkedList/SinglyCircular/SinglyCircularLinkedList.java
@@ -0,0 +1,85 @@
+public class SinglyCircularLinkedList {
+    Node head;
+
+    void insert(int data) {
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = newnode;
+            newnode.next = head;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        temp.next = newnode;
+        newnode.next = head;
+    }
+
+    void delete() {
+        if (head == null) {
+            return;
+        }
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != head) {
+            temp = temp.next;
+        }
+        temp.next = head;
+    }
+
+    void update(int oldval, int newval) {
+        if (head == null) {
+            System.out.println(oldval + " doesn't exist in list");
+            return;
+        }
+        Node temp = head;
+        do {
+            if (temp.data == oldval) {
+                temp.data = newval;
+                return;
+            }
+            temp = temp.next;
+        } while (temp != head);
+
+        System.out.println(oldval + " doesn't exist in list");
+    }
+
+    void printList(SinglyCircularLinkedList list) {
+        Node temp = list.head;
+        if (temp == null) {
+            System.out.println("Empty");
+            return;
+        }
+        do{
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }while (temp!= head);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SinglyCircularLinkedList list = new SinglyCircularLinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.insert(60);
+        list.insert(70);
+        list.insert(80);
+        list.printList(list);
+        System.out.println();
+
+        list.delete();
+        list.printList(list);
+        System.out.println();
+
+        list.update(20, 200);
+        list.printList(list);
+        System.out.println();
+    }
+}
