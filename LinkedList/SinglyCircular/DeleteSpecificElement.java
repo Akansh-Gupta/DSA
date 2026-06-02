20317afa9cf785eed6703a71a446c0b7f95c197e
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-02 23:24:24 +0530

    Added DeleteSpecificElement

---
+++ LinkedList/SinglyCircular/DeleteSpecificElement.java
@@ -0,0 +1,31 @@
+public class DeleteSpecificElement {
+    static void deleteSpecific(SinglyCircularLinkedList list, int val) {
+        Node temp = list.head;
+        while(temp.next!=list.head && temp.next.data!=val){
+            temp = temp.next;
+        }
+        if(temp == list.head){
+            System.out.println("Element doesn't exist in list");
+        }else{
+            temp.next = temp.next.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyCircularLinkedList list = new SinglyCircularLinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.insert(60);
+        list.insert(70);
+        list.insert(80);
+        list.insert(90);
+        list.insert(100);
+        list.printList(list);
+
+        deleteSpecific(list, 50);
+        list.printList(list);
+    }
+}
