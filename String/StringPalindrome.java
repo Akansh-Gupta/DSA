6daca96243ae4c2168728958f10d9adc32db8f1a
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-02 08:14:24 +0530

    Added StringPalindrome

--- String/StringPalindrome.java
+++ String/StringPalindrome.java
@@ -5,12 +5,12 @@ public class StringPalindrome {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the string: ");
         String str = sc.nextLine();
-        String rev = "";
+        StringBuilder rev = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
             char ch = str.charAt(i);
-            rev += ch;
+            rev.append(ch);
         }
-        if (str.equalsIgnoreCase(rev)) {
+        if (str.equalsIgnoreCase(rev.toString())) {
             System.out.println(str + " is a palindrome");
         }
         else{
