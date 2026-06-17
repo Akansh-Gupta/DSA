23ac4c905299056e1095c9e39335dc54a6fc8616
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-18 00:41:17 +0530

    Added TrappingRainwater

---
+++ Array/TrappingRainwater.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TrappingRainwater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println("Enter heights : ");
+        int height[] = new int[n];
+        for (int i = 0; i < n; i++)
+            height[i] = sc.nextInt();
+        if (n == 0) {
+            System.out.println("Total water : " + 0);
+            return;
+        }
+        int left[] = new int[n];
+        int right[] = new int[n];
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += Math.min(left[i], right[i]) - height[i];
+        }
+        System.out.println("Total water : " + total);
+        sc.close();
+    }
+}
