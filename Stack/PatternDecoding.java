274f97e59ee4248edee0a2dc907e94edb59b12a0
Akansh-Gupta <akg.gupta365@gmail.com>
2026-06-20 14:17:48 +0530

    Added PatternDecoding

--- Stack/PatternDecoding.java
+++ Stack/PatternDecoding.java
@@ -2,13 +2,39 @@ import java.util.*;
 
 public class PatternDecoding {
     public static void main(String[] args) {
-        StackLinkedList num = new StackLinkedList();
-        StackLinkedList alpha = new StackLinkedList();
+        Stack<StringBuilder> str = new Stack<>();
+        Stack<Integer> num = new Stack<>();
         Scanner sc = new Scanner(System.in);
-        // String str = sc.nextLine();
-        // for (int i = 0; i < str.length(); i++) {
-        //     Character ch = str.charAt(i);
-        // }
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        int n = 0;
+        for(char ch : s.toCharArray()){
+            if (Character.isDigit(ch)) {
+                n = n * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                num.push(n);
+                str.push(sb);
+                sb = new StringBuilder();
+                n = 0;
+            }
+            else if (ch == ']') {
+                int repeat = num.pop();
+                StringBuilder temp = str.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(sb);
+                }
+
+                sb = temp;
+            }
+            else {
+                sb.append(ch);
+            }
+        }
+
+        System.out.println(sb);
         sc.close();
     }
 }
